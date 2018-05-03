7e90a537ea9a47a48feed5fc0bc13c99b5239fa5
vishnu <samavardhan@gmail.com>
2018-05-02 20:43:25 -0500

    added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+	description = "AMI ID"
+	default = "ami-fe9a1884"
+}
+
+variable "instance_type" {
+	description = "instance type"
+	default = "t2.micro"
+}
+
+
+variable "key_name" {
+	description = "keyname"
+	default = "vishnu"
+}
