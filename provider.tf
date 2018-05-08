db6e3a325fc21f87f924c622f4f17daa08d9c4bf
samavardhan <samavardhan@gmail.com>
2018-05-07 19:02:43 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,11 @@ resource "aws_instance" "example"{
 ami = "${var.ami}",
 instance_type = "${var.instance_type}"
 key_name = "${var.key_name}"
+security_group = "${var.security_group}"
+vpc = "${var.vpc}"
 tags {
         Name = "first"
+        contact = "emailid"
     }
 #user_data = "${file("test.sh")}"
 }
