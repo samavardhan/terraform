131f48573a49636cade0e5bfd290dfa3ce83771f
samavardhan <samavardhan@gmail.com>
2018-05-07 19:08:51 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "example"{
 ami = "${var.ami}",
 instance_type = "${var.instance_type}"
 key_name = "${var.key_name}"
-security_groups = "${var.security_groups}"
-vpc_security_group_ids  = "${var.vpc_security_group_ids}"
+security_groups = ["${var.security_groups}"]
+vpc_security_group_ids  = ["${var.vpc_security_group_ids}"]
 tags {
         Name = "first"
         contact = "emailid"
