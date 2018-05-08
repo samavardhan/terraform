fd6d433effec2eafb20643851249b34221309236
samavardhan <samavardhan@gmail.com>
2018-05-07 19:10:13 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ ami = "${var.ami}",
 instance_type = "${var.instance_type}"
 key_name = "${var.key_name}"
 security_groups = ["${var.security_groups}"]
-vpc_security_group_ids  = ["${var.vpc_security_group_ids}"]
+#vpc_security_group_ids  = ["${var.vpc_security_group_ids}"]
 tags {
         Name = "first"
         contact = "emailid"
